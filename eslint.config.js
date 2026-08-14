02acfbb6afb40872180aec1576f545ba89dca2a3
Nicola Mustone <nicola@mustone.blog>
2026-08-14 18:56:42 +0300

    Build: drop the monorepo leftovers from the lint and blame config

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  // `site` and `docs` are self-contained Astro projects with their own tooling.
-  { ignores: ['dist', 'scripts', 'site', 'docs'] },
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
